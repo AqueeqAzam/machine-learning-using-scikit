80a3d55dee1690b06f41c6f67270780bc3fe8fe8
AQUEEQ AZAM <84491720+AqueeqAzam@users.noreply.github.com>
2024-07-13 21:31:37 +0530

    Update and rename data_labeling.py to 1_data_labeling.py

--- 1_data_labeling.py
+++ 1_data_labeling.py
@@ -8,29 +8,27 @@ print(input_labels)
 
 
 # Create and trained of label encoder object
-
-"""encoder = preprocessing.LabelEncoder()
+encoder = preprocessing.LabelEncoder()
 encoder.fit(input_labels)
-print(input_labels)"""
+print(input_labels)
 
 encoder = preprocessing.LabelEncoder()
 encoder.fit(input_labels)
 print(input_labels)
 
 
-"""test_labels = ['green', 'red', 'black']
+test_labels = ['green', 'red', 'black']
 encoded_values = encoder.transform(test_labels)
 print("\n label = ", test_labels)
-
 print("Encoded values =", list(encoded_values))
 
 
 test_labels = ['white', 'orange', 'green']
 encoded_values = encoder.transform(test_labels)
-print('Encoder values=', list(encoded_values))"""
+print('Encoder values=', list(encoded_values))
 
 # Decoding
-"""encoded_values = [3, 0, 4, 1]
+encoded_values = [3, 0, 4, 1]
 decoded_list = encoder.inverse_transform(encoded_values)
 print("\n Encoded values =", encoded_values)
 Encoded_values = [3, 0, 4, 1]
@@ -38,14 +36,14 @@ print("\n Decoded labels =", list(decoded_list))
 
 encoded_values = [3, 2, 0, 1]
 decoded_list = encoder.inverse_transform(encoded_values)
-print('\n Encoded values=', encoded_values)"""
+print('\n Encoded values=', encoded_values)
 
 # decoding a set of values
-"""encoded_values = [3,0,4,1]
+encoded_values = [3,0,4,1]
 decoded_list = encoder.inverse_transform(encoded_values)
 print("\nEncoded values =", encoded_values)
 
-print("\nDecoded labels =", list(decoded_list))"""
+print("\nDecoded labels =", list(decoded_list))
 
 encoder_values = [3, 2, 0, 1]
 decoder_lsit = encoder.inverse_transform(encoder_values)
